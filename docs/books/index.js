6e6b84f3ed8ae486f192ab8a0f23d9ba4959aaf3
Zulfikar31 <zulfikarrosadi31@gmail.com>
2021-11-09 21:11:17 +0700

    add new path to documenting new api endpoint

--- docs/books/index.js
+++ docs/books/index.js
@@ -1,11 +1,15 @@
 const getAllBooks = require('./getAllBooks');
 const createBook = require('./createBook');
+const getBookById = require('./getBookById');
 
 module.exports = {
   paths: {
     '/books': {
       ...getAllBooks,
       ...createBook,
+    },
+    '/books/{bookId}': {
+      ...getBookById,
     },
   },
 };
