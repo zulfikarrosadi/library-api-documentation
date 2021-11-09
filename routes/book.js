0c7c00dd1f5b3f244340b930805e55b53ebabaf9
Zulfikar31 <zulfikarrosadi31@gmail.com>
2021-11-09 20:41:25 +0700

    add new routes to get book by id

--- routes/book.js
+++ routes/book.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-const { getAllBooks, createBook } = require('../controllers/book');
+const { getAllBooks, getBookById, createBook } = require('../controllers/book');
 
 const router = Router();
 
 router.get('/', getAllBooks);
+router.get('/:id', getBookById);
 router.post('/', createBook);
 
 module.exports = router;
