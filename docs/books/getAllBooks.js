c55a457e54e660abda927ff0e8802eafc6478f0a
Zulfikar31 <zulfikarrosadi31@gmail.com>
2021-11-09 20:22:20 +0700

    rename file from getBooks to getAllBooks

---
+++ docs/books/getAllBooks.js
@@ -0,0 +1,32 @@
+module.exports = {
+  get: {
+    tags: ['Book'],
+    summary: 'Gett list of book',
+    description: '',
+    operationID: 'getAllBooks',
+    parameters: [],
+    requestBody: {},
+    responses: {
+      200: {
+        description: 'Successfully get all book data',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#components/schemes/SuccessBookAPIResponse',
+            },
+          },
+        },
+      },
+      400: {
+        description: 'Failed to get all book data',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#components/schemes/FailedBookAPIResponse',
+            },
+          },
+        },
+      },
+    },
+  },
+};
